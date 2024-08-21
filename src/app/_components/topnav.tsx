445f364add57e96fe386f2b59df46b8f9e629c55
Danish <dan71ish@gmail.com>
2024-08-21 09:09:13 +0530

    Upload thing added

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,6 +1,11 @@
+"use client";
 import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between border-b px-6 py-4 text-xl font-semibold">
       <div>Gallary</div>
@@ -9,6 +14,10 @@ export default function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => router.refresh()}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap items-center justify-center gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="w-full md:max-w-60">
+          <img src={image.url} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={index} className="w-full md:max-w-60">
-            <img src={image.url} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
